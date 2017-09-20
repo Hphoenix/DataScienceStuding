08d06ddab4d3ee83a5b3ec2a45410af887614001
Hphoenix <32027249+Hphoenix@users.noreply.github.com>
2017-09-20 17:06:11 +0800

    Update The Data Structure and Function of Program Language.cpp

--- The Data Structure and Function of Program Language.cpp
+++ The Data Structure and Function of Program Language.cpp
@@ -50,19 +50,44 @@ int LocateElem(SeqList L,DataType e)
 }
 
 int InsertList(SeqList *L,int i,DataType e)
+/*在顺序表的第i个位置插入元素e，插入成功返回1，如果插入位置不合法返回-1，顺序表满返回0*/
 {
   int j;
-  if(i<1||i>L->length+1)
+  if(i<1||i>L->length+1)  /*在插入元素之前，判断插入位置是否合法*/
   {
-    printf("插入位置i不合法！\n");
+    printf("插入位置i不合法！\n"); 
     return -1;
   }
-  else if(L-length>=ListSize)
+  else if(L-length>=ListSize) /*在插入元素之前，判断顺序表是否已满，不能插入元素*/
   {
-    for(j=L->length;j>=i;j==)
-      L->list[i-1]=e;
-    L-?list[i-1]=1;
-    L->length=L->length_1;
+    printf("顺序表已满，不能插入元素，\n");
+    return 0;
+  }
+  else
+  {
+    for(j=L->length;j>=i;j--) /*将第i个位置以后的元素依次后移*/
+      L->list[j]=L->list[j-1];
+    L->list[i-1]=e;           /*插入元素到第i个位置*/
+    L->length=L->length+1;    /*将顺序表长增1*/
     return 1;
   }
 }
+/*插入元素的位置i的合法范围应该是1≤i≤L->length+1。当i=1时，插入位置是在第1个元素之前；当i=L->length+1时，插入位置是最后一个元素之后，
+当插入位置是i=L->length+1时，不需要移动元素；当插入位置是i=0时，则需要移动所有元素。*/
+
+int DeleteList(SeqList *L,int i,DataType *e)
+{
+  int j;
+  if(L->length<=0)
+  {
+    printf("顺序表已空不能进行删除！\n");
+    return 0;
+  }
+  else if(i<1||i>L-length)
+  {
+    printf("删除位置不合适！\n");
+    return -1;
+  }
+  else
+  {
+    *e=L->
