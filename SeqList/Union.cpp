12802e96c3df21a4d6750e0b5547bb34a1aedccf
Hphoenix <32027249+Hphoenix@users.noreply.github.com>
2017-09-21 21:24:19 +0800

    Update Union.cpp

--- SeqList/Union.cpp
+++ SeqList/Union.cpp
@@ -1,4 +1,10 @@
 /*合并两个线性表中的元素（相同元素只保留一个）*/
 
 #include<stdio.h>
+#define ListSize 200
+typedef int DataType;   /*定义元素类型为整型*/
+#include"SeqList.h"	
+void UnionAB("SeqList *A,SeqList B"); /*将LB中但不在LA中的元素插入LA中*/
 
+void main()
+{
