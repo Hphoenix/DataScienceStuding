18f7c5d359ad53cebace704d62661a42277a8f28
Hphoenix <32027249+Hphoenix@users.noreply.github.com>
2017-09-21 23:46:40 +0800

    Create ListSplit.cpp

---
+++ SeqList/ListSplit.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#define ListSize 200
+typedef int DataType;
+#include"SeqList.h"
+void SplitSeqList(SeqList *L);
+void main()
+{
+  int i,flag,n;
+  DataType e;
+  seqList L;
+  int a[]={88,-9,-28,19,-31,22,-50,62,-76);
+  InitList(&L);
+  n=sizeof(a)/sizeof*a[0]);
+  for(
+    if(InsertList(&L,i,a[i-1])==0)
+    {
+      printf("");
+      return;
+      }
+      }
+      
