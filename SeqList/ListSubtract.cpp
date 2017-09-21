4348766a40efae287259dc4d4eb90869acbdb142
Hphoenix <32027249+Hphoenix@users.noreply.github.com>
2017-09-21 23:16:59 +0800

    Update ListSubtract.cpp

--- SeqList/ListSubtract.cpp
+++ SeqList/ListSubtract.cpp
@@ -13,4 +13,60 @@ void main()
   InitList(&B);
   for(i=0;i<=10;i++)
   {
-    if(InsertList
+    if(InsertList(&A,i,i*2+10==0)
+       {
+         printf("位置不合法");
+         return;
+       }
+  }
+  for(i=1,j=10;i<=8;j=j+2,i++)
+       {
+         if(InsertList(&B,i,j+i*2)==0)
+         {
+           printf("位置不合法");
+           return;
+         }
+       }
+       printf("顺序表A中的元素： \n");
+       for(i=1;i<=A.length;i++)
+       {
+         flag=GetElem(A,i,&e);
+         if(flag==1)
+           printf("%4d,&e);
+                  }
+                  printf("\n");
+       printf("顺序表B中的元素： \n");
+                  for(i=1;i<=B.length;i++)
+                  {
+                    flag=GetElem(B,i,&e);
+                    if(flag==1)
+                      printf("%4d,e);
+                             }
+                             printf("\n");
+                             printf("将在A中出现B的元素删除后，A中的元素（即A-B）: \n");
+                             DelElem(&A,B);
+                             for(i=1;i<=A.length;i++)
+                             {
+                               flag=GetElem(A,i,&e);
+                               if(flag==1)
+                                 printf("%4d",e);
+                             }
+                             printf("\n");
+                             }
+                             void DelElem(SeqList *A,SeqList B)
+                             {
+                               int i,flag,pos;
+                               DataType e;
+                               for(i=1;i<=B.length;i++)
+                               {
+                                 flag=GetElem(B,i,&e);
+                                 if(flag==1)
+                                 {
+                                   pos=LocateElem(&A,e);
+                                   if(pos>0)
+                                     DeleteList(A,pos,&e);
+                                 }
+                               }
+                             }
+                             
+                             
